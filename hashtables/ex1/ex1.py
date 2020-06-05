cc0bfc0669863b3a130a588bd77c146690cc1313
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-06-05 08:13:58 -0700

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for i in range(length):
+        cache[weights[i]] = i
+    
+    for i in range(length):
+        if (limit-weights[i]) in cache:
+            return [cache[limit-weights[i]],i]
+
+
+
 
     return None
