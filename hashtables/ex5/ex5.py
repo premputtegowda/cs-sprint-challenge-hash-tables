eb8bfd348563c778b30e6f89d5cab8646cc758a6
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-06-05 08:49:13 -0700

    stretch

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,20 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    cache={}
+    for file in files:
+
+        file_key = file.split("/")
+        if file_key[-1] not in cache:
+            cache[file_key[-1]] = [file]
+        else:
+            cache[file_key[-1]].append(file)
+    result=[]
+    for query in queries:
+        if query in cache:
+        
+            result = [*result, *cache[query]]
+            print(result)
 
     return result
 
